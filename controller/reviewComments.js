267759afc3673268a2d05c37b59fec963bba010d
ahnjaehun <77096665+anjaehun@users.noreply.github.com>
2022-07-06 16:11:07 +0900

    댓글(모집,장소추천,물품리뷰) 추가

--- controller/reviewComments.js
+++ controller/reviewComments.js
@@ -1,12 +1,105 @@
-const reviewPost = require("../schemas/reviewPost");
+const reviewPost = require("../schemas/reviewPost"); 
 const reviewComment = require("../schemas/reviewComment");
 const User = require("../schemas/user");
 
-// 육아용품 리뷰 댓글 등록
+// 모집 댓글 등록
 
-// 육아용품 리뷰 댓글 삭제
+async function reviewComments(req, res) {
+    try {
+        const { nickname } = res.locals.user;
+        const { reviewPostId } = req.params;
+        const { comment } = req.body;
+        let status = false;
+  
+       
+        
+        // 게시글 찾기 
+        const findPost = await reviewPost.findOne({ reviewPostId : Number(reviewPostId) });
+        console.log(findPost)
+        
+        if(!findPost){
+            res.status(400).send({
+                result: "false",
+                message: "게시글 번호가 없습니다 "
+            });
+        }
+         
+        console.log("reviewPostId:  "+findPost.reviewPostId)
+        // 게시글 작성
+        const reviewComments = await reviewComment.create({
+            nickname : nickname ,
+            reviewPostId : findPost.reviewPostId, 
+            comment : comment,
+            
+        });
+        console.log(reviewComments);
+  
+        
+
+        res.status(200).send({
+            result: "true",
+            message: "댓글이 성공적으로 등록되었습니다."
+        });
+
+       
+    } 
+    catch (err) {
+        res.status(400).send({
+            result: "false",
+            message: "댓글 작성 실패"
+        });
+    }
+};
+ 
+
+// 모집 댓글 삭제 
+async function reviewCommentsDelete(req, res) {
+    try {
+        const { reviewPostId, reviewCommentId } = req.params;
+        const { nickname } = res.locals.user;
+      
+        const reviewComments = await reviewComment.findOne({ 
+            reviewPostId: reviewPostId,
+            reviewCommentId: reviewCommentId,
+        });
+
+        if(!reviewComments ){
+            return res.status(200).send({  
+            result: "false",
+            message: "이미 지워진 댓글입니다."
+            });
+        };
+        
+        if (!(nickname==reviewComments.nickname)) {
+            return res.status(200).send({  
+                result: "false",
+                message: "닉네임이 일치하지 않습니다"
+            });
+        }  
+        
+        await reviewComment.deleteOne({ 
+                reviewPostId: reviewPostId,
+                reviewCommentId: reviewCommentId,
+            });
+       
+        
+        
+        
+
+        return res.status(200).send({
+            result: "true",
+            message: "댓글이 성공적으로 삭제되었습니다."
+        });
+         
+    } 
+    catch (err) {
+        res.status(400).send({
+            result: "false",
+            message: "알 수 없는 에러가 발생하였습니다"
+        });
+}};
 
 module.exports = {
     reviewComments,
-    reviewCommentsDelete,
+    reviewCommentsDelete
 };
--- routes/reviewComments.js
+++ routes/reviewComments.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const reviewCommentsController = require("../controller/reviewComments");
+const reviewCommentsController = require("../controller/reviewComments.js");
 const authMiddleware = require("../middlewares/authmiddleware");
 
-// 육아용품 리뷰 댓글 등록
-router.post("/reviews/:reviewPostId/comments",authMiddleware, reviewCommentsController.reviewComments);
+// 리뷰  댓글
+router.post("/reviews/:reviewPostId/comments"
+             ,authMiddleware, reviewCommentsController.reviewComments);
 
-// 육아용품 리뷰 댓글 삭제
-router.delete("/reviews/:reviewPostId/comments/:reviewCommentId",authMiddleware, reviewCommentsController.reviewCommentsDelete);
+ // 모집 댓글 삭제
+router.delete("/reviews/:reviewPostId/comments/:reviewCommentId"
+              ,authMiddleware, reviewCommentsController.reviewCommentsDelete);
 
 module.exports = router;
