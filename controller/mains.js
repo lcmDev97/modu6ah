e7ec5d3dfeead3e1832f18bb01b141054e105282
changmin97 <102282540+changmin97@users.noreply.github.com>
2022-07-07 00:05:36 +0900

    메인페이지 모집게시글 코드 작성

--- controller/mains.js
+++ controller/mains.js
@@ -4,9 +4,28 @@ const reviewPost = require("../schemas/reviewPost");
 
 // 메인 페이지 조회
 async function mainPostGet(req, res) {
+
+    //모집게시글
+    const recruitPosts = await recruitPost.find({ status: false }).limit(8).sort({ createdAt: -1 })
+    let remainNum;
+    if( recruitPosts.length !==8 ){
+        remainNum = 8 - recruitPosts.length
+    }
+    console.log(remainNum)
+    const truePosts = await recruitPost.find({ status: true }).limit(remainNum).sort({ createdAt: -1 })
+    for(let i = 0 ; i < remainNum ; i++){
+        recruitPosts.push(truePosts[i])
+    }
+    
+    //장소추천게시글
     const placeMain = await placePost.find({}).sort({ star: -1,  createdAt: -1 }.limit(2));
-    // console.log(placeMain);
-    res.status(200).send({placePosts: placeMain});
+
+
+    res.json({
+        result:true,
+        recruitPosts,
+        placePosts: placeMain
+    })
 };
 
 
