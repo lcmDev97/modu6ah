bc435c0fa06f25274255794e924b608acd4a267f
selim-jo <skyblue.777@daum.net>
2022-07-20 23:46:05 +0900

    [test] 테스트

--- controller/recruitPosts.js
+++ controller/recruitPosts.js
@@ -10,7 +10,7 @@ const moment = require("moment");
 
 // 모집 게시글 작성
 async function recruitPosts(req, res) {
-  try {
+//   try {
       // 불러올 정보 및 받아올 정보
       const { nickname, profileUrl } = res.locals.user;
     //   console.log(nickname)
@@ -39,12 +39,12 @@ async function recruitPosts(req, res) {
           result: "true",
           message: "게시글이 성공적으로 등록되었습니다."
       });
-  } catch (err) {
-      res.status(400).send({
-          result: "false",
-          message: "게시글 작성 실패"
-      });
-  }
+//   } catch (err) {
+//       res.status(400).send({
+//           result: "false",
+//           message: "게시글 작성 실패"
+//       });
+//   }
 };
 
 // 모집 게시글 전체조회
