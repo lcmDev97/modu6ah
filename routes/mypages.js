f5305f65fca16e7a2d614cf0403f2faa256505c4
changmin97 <102282540+changmin97@users.noreply.github.com>
2022-07-21 13:24:39 +0900

    북마크 카테고리별 단일 조회 라우터 생성

--- routes/mypages.js
+++ routes/mypages.js
@@ -7,8 +7,14 @@ const { profileUpload } = require('../middlewares/profileMulter');
 // 프로필 조회
 router.get("/mypage/profile/:nickname", mypagesController.profileGet);
 
-// 북마크 게시글 조회
+// 북마크 전체게시글 조회
 router.get("/mypage/bookmark", authMiddleware, mypagesController.myBookmark);
+// 북마크 모집게시글 조회
+router.get("/mypage/bookmark/recruits", authMiddleware, mypagesController.myBookmarkRecruit);
+// 북마크 장소추천게시글 조회
+router.get("/mypage/bookmark/places", authMiddleware, mypagesController.myBookmarkPlace);
+// 북마크 리뷰게시글 조회
+router.get("/mypage/bookmark/reviews", authMiddleware, mypagesController.myBookmarkReview);
 
 // 프로필 수정
 router.put("/mypage/update", authMiddleware, profileUpload.single('profileUrl'), mypagesController.profileUpdate);
