54740af2af82cdbb57ec6c8365d6b8653f9bb8ef
selim-jo <skyblue.777@daum.net>
2022-07-22 15:52:03 +0900

    [fix] 스웨거 주석처리

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ const passport = require("passport");
 const session = require("express-session");
 const passportConfig = require("./passport");
 const cookieParser = require("cookie-parser");
-const swaggerUi = require("swagger-ui-express");
-const swaggerFile = require("./swagger-output");
+// const swaggerUi = require("swagger-ui-express");
+// const swaggerFile = require("./swagger-output");
 
 // 소켓 관련 모듈
 const http = require('http');
@@ -72,7 +72,7 @@ app.use(
     [searchRouter]
 );
 app.use("/api/users", express.urlencoded({ extended: false }), [usersRouter]);
-app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerFile)); // 스웨거 파일
+// app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerFile)); // 스웨거 파일
 
 app.get("/", (req, res) => {
     res.send("redirect 테스트하기위한 루트 페이지입니다.");
