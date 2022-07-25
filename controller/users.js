efdbdea9d5bbca7ea3b0f9ad9a3cd561014000bf
changmin97 <102282540+changmin97@users.noreply.github.com>
2022-07-25 14:02:08 +0900

    이메일, 닉네임 중복체크 기능 구현

--- controller/users.js
+++ controller/users.js
@@ -7,29 +7,104 @@ const SALT_NUM = process.env.SALT_NUM;
 const SECRET_KEY = process.env.SECRET_KEY;
 const REFRESH_SECRET_KEY = process.env.REFRESH_SECRET_KEY;
 
-// 회원가입 조건
+// 회원가입 조이 스키마
 const signUpSchema = Joi.object({
-    email: Joi.string()
-        .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
-        .required()
-        .label("이메일 형식이 유효하지 않습니다."),
-    nickname: Joi.string()
+    email : Joi.string().required(),
+    nickname: Joi.string().required(),
+    password: Joi.string().pattern(new RegExp("^[0-9A-Za-z]{4,16}$")).required().label("비밀번호는 한글, 영문 대/소문자 4~16자 이여야 합니다."),
+    passwordCheck: Joi.string(),
+});
+// 이메일 조이 스키마
+const emailSchema = Joi.object({
+  email: Joi.string()
+  .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+  .required()
+  .label("이메일 형식이 유효하지 않습니다."),
+})
+// 닉네임 조이 스키마
+const nicknameSchema = Joi.object({
+  nickname: Joi.string()
         .pattern(new RegExp("^[0-9A-Za-z가-힣]{2,12}$"))
         .required()
         .label("닉네임은 한글, 영문 대/소문자, 숫자, 2~12자 이여야 합니다."),
-    password: Joi.string()
-        .pattern(new RegExp("^[0-9A-Za-z]{4,16}$"))
-        .required()
-        .label("비밀번호는 한글, 영문 대/소문자 4~16자 이여야 합니다."),
-    passwordCheck: Joi.string(),
-});
+})
+
+async function emailCheck(req, res, next) {
+  try{
+    const email = req.body.email
+    //#1.입력하지 않았을경우
+    if(!email){
+      return res.status(400).json({
+        result : false,
+        message : "사용할 이메일을 입력해 주세요."
+      })
+    }
+    //#2.이메일 형식검사
+    await emailSchema.validateAsync(req.body);
+    //#3.이메일 중복확인
+    const existEail = await User.findOne({ email })
+    if(existEail){
+      return res.status(409).json({
+        result : false,
+        message : "사용중인 이메일 입니다."
+      })
+    }
+
+    return res.json({
+      result : true,
+      message : "사용가능한 이메일 입니다."
+    })
+
+  }catch(err){
+    return res.status(400).json({
+      result : false,
+      message : err.details[0].context.label,
+    })
+  }
+  
+}
+
+
+async function nicknameCheck(req, res, next) {
+  
+  try{
+    const {nickname} = req.body
+    //#1.입력하지 않았을경우
+    if(!nickname){
+      return res.status(400).json({
+        result : false,
+        message : "사용할 닉네임을 입력해 주세요."
+      })
+    }
+    //#2.이메일 형식검사
+    await nicknameSchema.validateAsync(req.body);
+    //#3.이메일 중복확인
+    const existEail = await User.findOne({ nickname })
+    if(existEail){
+      return res.status(409).json({
+        result : false,
+        message : "사용중인 닉네임 입니다."
+      })
+    }
+
+    return res.json({
+      result : true,
+      message : "사용가능한 닉네임 입니다."
+    })
+
+  }catch(err){
+    return res.status(400).json({
+      result : false,
+      message : err.details[0].context.label,
+    })
+  }
+
+}
 
 async function signup(req, res, next) {
     try {
-        const { email, nickname, password, passwordCheck } =
-            await signUpSchema.validateAsync(req.body);
+        const {email, nickname, password, passwordCheck } = await signUpSchema.validateAsync(req.body);
         const existUser = await User.findOne({ email });
-        // console.log('existUser정보',existUser)
         //이미 이메일이 존재하는 경우
         if (existUser) {
             return res.status(400).json({
@@ -52,7 +127,6 @@ async function signup(req, res, next) {
                 result: false,
             });
         }
-
         const salt = await Bcrypt.genSalt(Number(SALT_NUM));
         const hashPassword = await Bcrypt.hash(password, salt); // 비밀번호 암호화
         const user = new User({
@@ -216,5 +290,7 @@ module.exports = {
     signup,
     signin,
     kakao_member,
-    kakao_parsing
+    kakao_parsing,
+    emailCheck,
+    nicknameCheck,
 };
--- routes/users.js
+++ routes/users.js
@@ -1,38 +1,16 @@
 require("dotenv").config();
 const express = require("express");
-const passport = require('passport');
 const router = express.Router();
 const userController = require("../controller/users")
-const { isLoggedIn, isNotLoggedIn } = require('./middlewares') // 인증미들웨어2(미들웨어1 작동 안되면 이거쓰기)
-const authmiddleware = require('../middlewares/authmiddleware')
-const jwt = require("jsonwebtoken");
 
 // 회원가입
-router.post("/signup", userController.signup)  //auth미들웨어 붙이기
+router.post("/signup", userController.signup)
 
-router.post("/signin", userController.signin) //auth미들웨어 붙이기
+router.post("/signin", userController.signin)
 
-// router.get('/kakao', passport.authenticate('kakao'));
+router.post("/signup/emailCheck", userController.emailCheck)
 
-// router.get('/kakao/callback', passport.authenticate('kakao', {
-//   failureRedirect: '/api/main',
-// }), (req, res) => {
-//   const nickname = req.user.nickname
-//   const profileUrl = req.user.profileUrl
-//   const accessToken = jwt.sign({ nickname }, process.env.SECRET_KEY, {
-//       expiresIn: "4h",
-//     });
-
-//   res.json({
-//     result : true,
-//     accessToken,
-//     nickname,
-//     profileUrl
-//    }
-//   )
-// });
-//앞에 api/users/붙어있어요!
-// router.post('/auth/login/kakao/callback', userController.kakao_callback);
+router.post("/signup/nicknameCheck", userController.nicknameCheck)
 
 router.post('/kakao/member', userController.kakao_member);
 
