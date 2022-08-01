12f9d576c072dcb7b7f0cd7c95971fb91d8f3779
selim-jo <skyblue.777@daum.net>
2022-08-01 11:03:18 +0900

    [test] 소켓 테스트

--- controller/chatMessages.js
+++ controller/chatMessages.js
@@ -8,12 +8,13 @@ async function chatMessagesGet(req, res) {
     try{
         const { roomId } = req.params;
         const chatMessageList = await chatMessage.find({ roomId: Number(roomId) });
+        const titleRoom = await chatRoom.findOne({roomId:Number(roomId)});
         // console.log(chatMessageList);
 
         if (!chatMessageList[0]) {
-            return res.status(400).send({ result: "false", message: "해당 채팅방의 메시지가 존재하지 않습니다."});
+            return res.status(400).send({ result: "false", message: "해당 채팅방의 메시지가 존재하지 않습니다.", titleRoom : titleRoom.postTitle});
         } else {
-            return res.status(200).send({ chatMessageList: chatMessageList });
+            return res.status(200).send({ chatMessageList: chatMessageList, titleRoom : titleRoom.postTitle });
         }
     } catch (err) {
         return res.status(400).send({
